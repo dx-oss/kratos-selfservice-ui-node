d017a2454fa373c5c217c8e31add278249f8c474
afreakk <laderud@hotmail.com>
2023-11-20 17:49:51 +0100

    update otel libs n stuff

--- src/bootstrap.ts
+++ src/bootstrap.ts
@@ -1,18 +1,23 @@
 // tracing.ts
-import { NodeSDK } from '@opentelemetry/sdk-node';
-import { HoneycombSDK } from '@honeycombio/opentelemetry-node';
-import { getNodeAutoInstrumentations } from '@opentelemetry/auto-instrumentations-node';
+import { HoneycombSDK } from "@honeycombio/opentelemetry-node"
+import { getNodeAutoInstrumentations } from "@opentelemetry/auto-instrumentations-node"
+import { NodeSDK } from "@opentelemetry/sdk-node"
 
-// uses HONEYCOMB_API_KEY and OTEL_SERVICE_NAME environment variables
+// Uses environment variables named HONEYCOMB_API_KEY and OTEL_SERVICE_NAME
 const sdk: NodeSDK = new HoneycombSDK({
-  instrumentations: [getNodeAutoInstrumentations()],
-});
+  instrumentations: [
+    getNodeAutoInstrumentations({
+      // We recommend disabling fs automatic instrumentation because
+      // it can be noisy and expensive during startup
+      "@opentelemetry/instrumentation-fs": {
+        enabled: false 
+      },
+    }),
+  ],
+})
 
-sdk
-  .start()
-  .then(() => {
-    console.log('Tracing initialized');
-  })
-  .catch((error) => console.log('Error initializing tracing', error));
-
+sdk.start()
+// this needs to be at the bottom, so we bootstrap telemetry before evaluating index
+// @ts-ignore
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
 import "./index"
